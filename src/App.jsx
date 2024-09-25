c5e7b0f3ace2de0d3e683fcf7e90692110c17b79
Felipe <devfbraunstein@gmail.com>
2024-09-25 17:11:34 -0300

    Footer basico feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import Nav from './components/Nav'
-import Footer from './components/Footer'
-import { Outlet } from 'react-router-dom'
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  
-
   return (
-    <>
-    <Nav/>
-    <Outlet/>
-    <Footer/>
-    </>
-  )
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      {/* Navbar no topo */}
+      <Nav />
+      
+      {/* Outlet (conteúdo da página) ocupa o espaço flexível */}
+      <div style={{ flexGrow: 1 }}>
+        <Outlet />
+      </div>
+      
+      {/* Footer no final da página */}
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
-const Footer=()=>{
-    return(
-        <>
-        <h5>@2024-Direitos reservados</h5>
-        </>
-    )
+const Footer = () => {
+    return (
+        <footer style={footerStyle}>
+            <h5>@2024 - Direitos reservados</h5>
+        </footer>
+    );
 }
 
-export default Footer
+const footerStyle = {
+    width: '100%',
+    height: '100px', // Ajuste a altura conforme necessário
+    backgroundColor: '#4d4d4d',
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    bottom: '0',
+    textAlign: 'center',
+};
+
+export default Footer;
